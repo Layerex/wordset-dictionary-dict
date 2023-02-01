09a26710e920dac6c17d07d7cee6b4458721832f
Layerex <layerex@dismail.de>
2023-02-01 17:26:31 +0300

    Normalize data

--- wordset-dictionary-parse.py
+++ wordset-dictionary-parse.py
@@ -3,6 +3,17 @@
 import glob
 import json
 import sys
+from typing import Callable
+
+
+def end_sentence_with_dot(s: str) -> str:
+    if not s[-1] in (".", "?", "!"):
+        s += "."
+    return s
+
+
+def modify_first_letter(f: Callable[[str], str], s: str) -> str:
+    return f(s[0]) + s[1:]
 
 
 def main():
@@ -21,15 +32,23 @@ def main():
                 for word in words.values():
                     if "meanings" in word.keys():
                         for meaning in word["meanings"]:
+                            meaning["def"] = modify_first_letter(
+                                lambda s: s.lower(),
+                                end_sentence_with_dot(meaning["def"].strip()),
+                            )
                             out.write(
-                                f':{word["word"]}:({meaning["speech_part"]})' f' {meaning["def"]}.'
+                                f':{word["word"]}:({meaning["speech_part"]}) {meaning["def"]}'
                             )
                             if "example" in meaning.keys():
+                                meaning["example"] = modify_first_letter(
+                                    lambda s: s.capitalize(),
+                                    end_sentence_with_dot(meaning["example"].strip()),
+                                )
                                 out.write(f' {meaning["example"]}')
                             if "synonyms" in meaning.keys():
                                 out.write(
-                                    f" Synonyms:"
-                                    f' {" ".join((synonym for synonym in meaning["synonyms"] if synonym is not None))}.'
+                                    f"\nSynonyms:"
+                                    f' {", ".join((synonym for synonym in meaning["synonyms"] if synonym is not None))}.'
                                 )
                             out.write("\n")
 
